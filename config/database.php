5d465710f1504713f9965d4d9a1786d44dbf4d79
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-08-01 15:26:28 +1000

    fix DB

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'manillaglass'),
+			'database'  => env('DB_DATABASE', 'wswindows'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', 'L4C62vjnRDWCEfC7CIgW'),
 			'charset'   => 'utf8',
